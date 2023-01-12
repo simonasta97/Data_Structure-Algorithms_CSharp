105f78e06a31defe85e61cd140a72135e1aff16e
simonasta <simonasta@abv.bg>
2023-01-12 15:11:33 +0200

    Stack dynamic implementation

--- Linear Data Structures/Problem02.Stack/Stack.cs
+++ Linear Data Structures/Problem02.Stack/Stack.cs
@@ -8,41 +8,79 @@
     {
         private class Node
         {
-            
+            public T Element { get; set; }
+
+            public Node Next { get; set; }
+
+            public Node(T element, Node next)
+            {
+                Element = element;
+                Next = next;
+            }
+
+            public Node(T element)
+            {
+                Element = element;
+            }
         }
 
         private Node top;
 
-        public int Count => throw new System.NotImplementedException();
+        public int Count { get; private set; }
 
         public void Push(T item)
         {
-            throw new NotImplementedException();
+            var node = new Node(item, this.top);
+            this.top = node;
+            this.Count++;
         }
 
         public T Pop()
         {
-            throw new System.NotImplementedException();
+            if (this.top == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var oldTop = this.top;
+            this.Count--;
+            this.top = oldTop.Next;
+            return oldTop.Element;
         }
 
         public T Peek()
         {
-            throw new System.NotImplementedException();
+            if (this.top == null)
+            {
+                throw new InvalidOperationException();
+            }
+            return this.top.Element;
         }
 
         public bool Contains(T item)
         {
-            throw new System.NotImplementedException();
+            var top = this.top;
+            while (top != null)
+            {
+                if (top.Element.Equals(item))
+                {
+                    return true;
+                }
+                top = top.Next;
+            }
+            return false;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            var node = this.top;
+            while (node != null)
+            {
+                yield return node.Element;
+                node = node.Next;
+            }
         }
 
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            throw new NotImplementedException();
-        }
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
     }
 }
