c1a1aa1a41c8a0f673e2ddb51471c23e13e7d054
simonasta <simonasta@abv.bg>
2023-01-13 20:59:39 +0200

    Circular Queue - implement

---
+++ Linear-Data-Structures-Exercise/01.FasterQueue/CircularQueue.cs
@@ -0,0 +1,72 @@
+﻿namespace Problem01.CircularQueue
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class CircularQueue<T> : IAbstractQueue<T>
+    {
+        private T[] elements;
+        private int startIndex, endIndex;
+        public CircularQueue(int capacity = 4)
+        {
+            this.elements = new T[capacity];
+        }
+
+        public int Count { get; set; }
+
+        public T Dequeue()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Enqueue(T item)
+        {
+            if (this.Count >= this.elements.Length)
+            {
+                this.Grow();
+            }
+            this.elements[endIndex] = item;
+            this.endIndex = (this.endIndex + 1) % this.elements.Length;
+            this.Count++;
+        }
+
+        public T Peek()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T[] ToArray()
+        {
+            return this.CopyElements(new T[this.Count]);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int currentIndex = 0; currentIndex < this.Count; currentIndex++)
+            {
+                yield return this.elements[(startIndex + currentIndex) % this.elements.Length];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+
+        private void Grow()
+        {
+            this.elements = this.CopyElements(new T[this.elements.Length * 2]);
+            this.startIndex = 0;
+            this.endIndex = this.Count;
+        }
+
+        private T[] CopyElements(T[] resultArr)
+        {
+            for (int currentIndex = 0; currentIndex < this.Count; currentIndex++)
+            {
+                resultArr[currentIndex] = this.elements[(this.startIndex + currentIndex) % this.elements.Length];
+            }
+
+            return resultArr;
+        } 
+    }
+
+}
