d37efdadf01026d8049b49f62dec750d74b09d69
simonasta <simonasta@abv.bg>
2023-01-31 12:46:50 +0200

    Implement Tree Factory

---
+++ Linear Data Structures-Lab/Problem03.Queue/Queue.cs
@@ -0,0 +1,98 @@
+﻿namespace Problem03.Queue
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Threading;
+
+    public class Queue<T> : IAbstractQueue<T>
+    {
+        private class Node
+        {
+            public T Element { get; set; }
+
+            public Node Next { get; set; }
+
+            public Node(T element, Node next)
+            {
+                Element = element;
+                Next = next;
+            }
+
+            public Node(T element)
+            {
+                Element = element;
+            }
+        }
+
+        private Node head;
+
+        public int Count { get; private set; }
+
+        public void Enqueue(T item)
+        {
+            var newNode = new Node(item);
+            if (this.head == null)
+            {
+                this.head = newNode;
+            }
+            else
+            {
+                var node = this.head;
+                while (node.Next != null)
+                {
+                    node = node.Next;
+                }
+                node.Next = newNode;
+            }
+            this.Count++;
+        }
+
+        public T Dequeue()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            var oldHead = this.head;
+            this.head = this.head.Next;
+            Count--;
+            return oldHead.Element;
+        }
+
+        public T Peek()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            return this.head.Element;
+        }
+
+        public bool Contains(T item)
+        {
+            var head = this.head;
+            while (head != null)
+            {
+                if (head.Element.Equals(item))
+                {
+                    return true;
+                }
+                head = head.Next;
+            }
+            return false;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var node = this.head;
+            while (node != null)
+            {
+                yield return node.Element;
+                node = node.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+    }
+}
---
+++ Trees-Representation-and-Traversal-(BFS-DFS)-Exercise/Tree/Tree.cs
@@ -0,0 +1,61 @@
+﻿namespace Tree
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Tree<T> : IAbstractTree<T>
+    {
+        private readonly List<Tree<T>> children;
+        public Tree(T key, params Tree<T>[] children)
+        {
+            this.Key = key;
+            this.children = new List<Tree<T>>();
+
+            foreach (var child in children)
+            {
+                this.AddChild(child);
+            }
+        }
+
+        public T Key { get; private set; }
+
+        public Tree<T> Parent { get; private set; }
+
+        public IReadOnlyCollection<Tree<T>> Children => this.children.AsReadOnly();
+
+        public void AddChild(Tree<T> child)
+        {
+            this.children.Add(child);
+        }
+
+        public void AddParent(Tree<T> parent)
+        {
+            this.Parent = parent;
+        }
+
+        public string AsString()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetInternalKeys()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetLeafKeys()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetDeepestKey()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetLongestPath()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
